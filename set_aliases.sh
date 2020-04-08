672b61f704862aed1c5fab149d00fa4e803c5f6b
satken2 <developer1@test.com>
2020-04-08 21:45:56 +0900

    Fixed wring container name

--- set_aliases.sh
+++ set_aliases.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 alias sudo='sudo '
-alias ansible='docker run -v "${PWD}":/work:ro --rm satken2/docker-ansible ansible'
-alias ansible-playbook='docker run -v "${PWD}":/work:ro -v ~/.ansible/roles:/root/.ansible/roles -v ~/.ssh:/root/.ssh:ro --rm satken2/docker-ansible ansible-playbook'
-alias ansible-galaxy='docker run -v ~/.ansible/roles:/root/.ansible/roles --rm satken2/docker-ansible ansible-galaxy'
-alias ansible-vault='docker run -v "${PWD}":/work:ro --rm satken2/docker-ansible ansible-vault'
+alias ansible='docker run -v "${PWD}":/work:ro --rm satken2/ansible ansible'
+alias ansible-playbook='docker run -v "${PWD}":/work:ro -v ~/.ansible/roles:/root/.ansible/roles -v ~/.ssh:/root/.ssh:ro --rm satken2/ansible ansible-playbook'
+alias ansible-galaxy='docker run -v ~/.ansible/roles:/root/.ansible/roles --rm satken2/ansible ansible-galaxy'
+alias ansible-vault='docker run -v "${PWD}":/work --rm satken2/ansible ansible-vault'
