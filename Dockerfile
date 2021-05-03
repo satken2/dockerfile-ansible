b3fea5e9320603b247a2e4e9fd4862be1154326c
satken2 <developer1@test.com>
2021-05-03 14:49:02 +0900

    Updated to 3.3.0 and specified package versions

--- Dockerfile
+++
@@ -1,4 +0,0 @@
-FROM python:3.9.2-alpine
-RUN apk update && apk add gcc libc-dev make git libffi-dev openssl-dev python3-dev libxml2-dev libxslt-dev openssh-client sshpass
-RUN pip install 'cryptography==2.8'
-RUN pip install ansible ansible-lint
--- set_aliases.sh
+++ set_aliases.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 alias sudo='sudo '
-alias ansible='docker run -v "${PWD}":/work:ro --rm satken2/ansible ansible'
-alias ansible-playbook='docker run -v "${PWD}":/work:ro -v ~/.ansible/roles:/root/.ansible/roles -v ~/.ssh:/root/.ssh:ro --rm satken2/ansible ansible-playbook'
+alias ansible='docker run -v "${PWD}":/work:ro -w /work --rm satken2/ansible ansible'
+alias ansible-playbook='docker run -v "${PWD}":/work:ro -w /work -v ~/.ansible/roles:/root/.ansible/roles -v ~/.ssh:/root/.ssh:ro --rm satken2/ansible ansible-playbook'
 alias ansible-galaxy='docker run -v ~/.ansible/roles:/root/.ansible/roles --rm satken2/ansible ansible-galaxy'
-alias ansible-vault='docker run -v "${PWD}":/work --rm satken2/ansible ansible-vault'
-alias ansible-lint='docker run -v "${PWD}":/work --rm satken2/ansible ansible-lint'
+alias ansible-vault='docker run -v "${PWD}":/work -w /work --rm satken2/ansible ansible-vault'
+alias ansible-lint='docker run -v "${PWD}":/work -w /work --rm satken2/ansible ansible-lint'
